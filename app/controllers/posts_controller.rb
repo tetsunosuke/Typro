f35b6649f1fc7ec2c61359facb0994d050ecfc2a
shiropt <63380875+shiropt@users.noreply.github.com>
2020-06-01 09:26:21 +0900

    不要なアクション削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,25 +22,8 @@ class PostsController < ApplicationController
     @post =  Post.find(params[:id])
   end
   
-  def edit
-    @post = Post.find(params[:id])
-    
-  end
   
-  def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-    redirect_to post_path(post.id)
-
- end
-
- def destroy
-  post = Post.find(params[:id])
-  post.destroy
-  redirect_to root_path
-
- end
-
+  
   
   private
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
-  resources :posts, except: :index
+  resources :posts, only: [:index,:new,:create]
   resources :users, only: :show
 
 end
