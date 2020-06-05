31f02280b6a801c4e67c469ec9ff9ca033e0d2e6
shiropt <63380875+shiropt@users.noreply.github.com>
2020-06-05 11:22:05 +0900

    不要なファイルの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,2 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @posts = @user.posts
-    @newscore =  @posts.order("created_at DESC").first(10)
-    @hiscore =  @posts.order("score DESC").first(10)
- end
 end
