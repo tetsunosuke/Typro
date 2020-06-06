57dee8e5cd7f4822dd299bccb16c661634741b7c
shiropt <63380875+shiropt@users.noreply.github.com>
2020-06-06 22:09:44 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.0"
 
 set :application, "simpleisbest"
-set :repo_url, "git@example.com:shiropt/typ-pro.git"
+set :repo_url, "git@example.com:shiropt/Typro.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
